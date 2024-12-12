62b010cfddb69299f961a583ac4d037ec1496ad9
Furkan Abay <furkan_abay@hotmail.com>
2024-12-12 03:12:27 +0100

    feat: removed padding X

--- src/components/sections/smallerSections/Comparisons.tsx
+++ src/components/sections/smallerSections/Comparisons.tsx
@@ -31,7 +31,7 @@ export default function Comparisons({ comparisons, loanAmount, binding_period, m
     <>
       <hr className="mt-10 mb-8 mt:my-4" />
       <h4 className="font-semibold text-2xl">Jämför vår ränta med andra banker</h4>
-      <ul className="flex mt-6 space-x-5 pb-4 px-4 overflow-scroll relative
+      <ul className="flex mt-6 space-x-5 pb-4 overflow-scroll relative
         [&::-webkit-scrollbar]:h-2
         [&::-webkit-scrollbar]:z-40
         [&::-webkit-scrollbar-track]:rounded-full
@@ -42,7 +42,12 @@ export default function Comparisons({ comparisons, loanAmount, binding_period, m
         {...events}
         ref={ref}
       >
-        {banks_array.length > 0 ? banks_array : <p>Det finns inga banker att jämföra med för denna bindningstid. Välj en annan bindningstid för att se jämförelser.</p>}
+        {banks_array.length > 0 ?
+          banks_array :
+          <p>
+            Det finns inga banker att jämföra med för denna bindningstid. Välj en annan bindningstid för att se jämförelser.
+          </p>
+        }
       </ul>
     </>
   )
